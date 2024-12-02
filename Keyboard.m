17f585c99cab533dff2421beb42a5a70857f711f
Abhronil Dutta <abhronil.dutta2006@gmail.com>
2024-12-02 23:39:04 +0530

    Update Keyboard.m

--- Keyboard.m
+++ Keyboard.m
@@ -16,50 +16,46 @@ sidespeed = 50;
 pickupspeed = 15;
 
 brick.SetColorMode(1, 2);
-manual = true;
 InitKeyboard();
 
 while 1
     pause(0.1);
     %keyboard control 
-    if manual == true
-        switch key
-            case 'w'
-                disp("W is pressed");
-                brick.MoveMotor('AD', frontspeed);
-            case 's'
-                disp("S is pressed");
-                brick.MoveMotor('AD', -backspeed);
-            case 'a'
-                disp("A is pressed");
-                brick.MoveMotor('A', -sidespeed);
-                brick.MoveMotor('D', sidespeed);
-            case 'd'
-                disp("D is pressed");
-                brick.MoveMotor('D', -sidespeed);
-                brick.MoveMotor('A', sidespeed);
-            case 0
-                disp('No key pressed');
-                brick.StopMotor('ADC', 'Brake');
-            case 'q'
-                disp("Quit");
-                brick.StopMotor('ADB', 'Brake');
-                manual = false;
-                run("Automatic.m");
-                break;
+    switch key
+        case 'w'
+            disp("W is pressed");
+            brick.MoveMotor('AD', frontspeed);
+        case 's'
+            disp("S is pressed");
+            brick.MoveMotor('AD', -backspeed);
+        case 'a'
+            disp("A is pressed");
+            brick.MoveMotor('A', -sidespeed);
+            brick.MoveMotor('D', sidespeed);
+        case 'd'
+            disp("D is pressed");
+            brick.MoveMotor('D', -sidespeed);
+            brick.MoveMotor('A', sidespeed);
+        case 0
+            disp('No key pressed');
+            brick.StopMotor('ADC', 'Brake');
+        case 'q'
+            disp("Quit");
+            brick.StopMotor('ADB', 'Brake');
+            run("Automatic.m");
+            break;
                 
-            % pickup
-            case 'i' 
-                disp("UP");
-                brick.MoveMotor('B', -pickupspeed);
-                pause(0.1);
-                brick.StopMotor('B', 'Brake');
-            case 'k'
-                disp("DOWN");
-                brick.MoveMotor('B', pickupspeed);
-                pause(0.1);
-                brick.StopMotor('B', 'Brake');
-        end
+        % pickup
+        case 'i' 
+            disp("UP");
+            brick.MoveMotor('B', -pickupspeed);
+            pause(0.1);
+            brick.StopMotor('B', 'Brake');
+        case 'k'
+            disp("DOWN");
+            brick.MoveMotor('B', pickupspeed);
+            pause(0.1);
+            brick.StopMotor('B', 'Brake');
     end
 end
 CloseKeyboard();
